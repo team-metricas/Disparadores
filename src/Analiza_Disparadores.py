0a02b2c682bdfa3341155ee1ce1631ee6f182716
edveralli <edveralli@gmail.com>
2024-07-18 12:25:41 -0300

    largo de Frases y Palabras Totales

--- src/Analiza_Disparadores.py
+++ src/Analiza_Disparadores.py
@@ -3,6 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Si no tienes las stopwords de NLTK, necesitas descargarlas
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# Definir stop words en español
+stop_words_es = set(stopwords.words('spanish'))
 
 # Cambio al directorio de datos
 os.chdir("../data/")
@@ -13,6 +23,50 @@ df_tsv = df_tsv.dropna(subset=['Keywords'])
 
 # Elimino filas donde la columna "Active" es booleana y tiene valor FALSE
 df_tsv = df_tsv[df_tsv['Active'] != False]
+
+# Agrego una columna con la cantidad de veces que se encontró la cadena "\r\n" en "Keywords" y le sumo 1
+df_tsv['frases_por_intencion'] = df_tsv['Keywords'].str.count(re.escape(r'\r\n')) + 1
+
+# Ordeno el DataFrame por 'frases_por_intencion' en orden decreciente
+df_tsv_sorted = df_tsv.sort_values(by='frases_por_intencion', ascending=False)
+
+# Exporto el DataFrame con las columnas "Name", "ID", "Keywords" y "frases_por_intencion" en orden decreciente
+df_tsv_sorted[['Name', 'ID', 'Keywords', 'frases_por_intencion']].to_csv("../frases_por_intencion.csv", sep=';', index=False, encoding='utf-8-sig')
+
+# Función para eliminar stop words y limpiar caracteres no deseados
+def clean_and_remove_stopwords(text):
+    # Reemplazar caracteres no deseados por un espacio en blanco, excepto \r\n
+    text = re.sub(r'[().,|?!¡¿]', ' ', text)
+    # Tokenizar el texto
+    words = word_tokenize(text, language='spanish')
+    # Eliminar stop words
+    words_filtered = [word for word in words if word.lower() not in stop_words_es]
+    return ' '.join(words_filtered)
+
+# Creo una nueva columna "Keywords_limpias" eliminando las stop words y limpiando caracteres no deseados
+df_tsv['Keywords_limpias'] = df_tsv['Keywords'].apply(clean_and_remove_stopwords)
+
+# Calculo la cantidad de palabras totales en "Keywords_limpias"
+df_tsv['word_count'] = df_tsv['Keywords_limpias'].apply(lambda x: len(word_tokenize(x, language='spanish')))
+
+# Ordeno el DataFrame por 'word_count' en orden decreciente
+df_tsv_sorted = df_tsv.sort_values(by='word_count', ascending=False)
+
+# Exporto el DataFrame ordenado a un archivo CSV
+df_tsv_sorted.to_csv("../df_tsv_sorted.csv", sep=';', index=False, encoding='utf-8-sig')
+
+# Suma de todos los valores en la columna 'word_count'
+total_word_count = df_tsv_sorted['word_count'].sum()
+
+# Generar un gráfico que muestre el número total de palabras
+plt.figure(figsize=(8, 6))
+plt.text(0.5, 0.5, f'Total de palabras:\n{total_word_count}', fontsize=16, ha='center', va='center')
+plt.axis('off')  # Desactivar ejes para que solo muestre el texto
+plt.savefig("../total_word_count.png", bbox_inches='tight', pad_inches=0.1)  # Guardar la imagen como PNG
+plt.show()
+
+#  FIN de calculo cantidad de palabras y cantidad de frases por intencion
+
 
 # Creo una nueva columna con la lista de palabras, limpiando delimitadores y reemplazando \r\n por un espacio en blanco
 df_tsv['keywords_list'] = df_tsv['Keywords'].apply(
@@ -101,7 +155,7 @@ plt.xlabel('Disparador')
 plt.ylabel('Frecuencia')
 plt.title('Curva de Frecuencia Total de Disparadores')
 plt.xticks(rotation=45, ha='right')
-plt.tight_layout()
+#plt.tight_layout()
 plt.savefig("../curva_frecuencia_total.png")
 plt.show()
 
@@ -114,8 +168,6 @@ median_value = round(frequency_analysis['frecuencia'].median(), 2)
 
 # Agrego la mediana a los descriptores estadísticos, no me venia en el paquete
 descriptive_stats['median'] = median_value
-
-print(descriptive_stats)
 
 # Exporto los descriptores estadísticos a un archivo CSV
 descriptive_stats.to_csv("../descriptive_stats.csv", sep=';', encoding='utf-8-sig')
@@ -135,9 +187,6 @@ top_30_longest = expanded_df_sorted.head(30)
 # Exporto las 30 primeras filas a un archivo CSV
 top_30_longest[['ID', 'Name', 'disparador', 'length', 'ranking']].to_csv("../top_30_longest_disparadores.csv", sep=';', index=False, encoding='utf-8-sig')
 
-# Veo el ranking de las cadenas más largas
-print(top_30_longest[['disparador', 'length', 'ranking']])
-
 
 """
 Percentiles: Los percentiles dividen un conjunto de datos en 100 partes iguales. El valor en el percentil 
@@ -145,3 +194,5 @@ Percentiles: Los percentiles dividen un conjunto de datos en 100 partes iguales.
 En otras palabras, si un valor se encuentra en el percentil 25,
 significa que el 25% de los datos son menores o iguales a ese valor.
 """
+
+
